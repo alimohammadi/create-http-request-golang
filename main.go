2f7a664d3a36410ef4d23d4721fec42dd58b2ae1
Ali Mohammadi <alimohammadi02321@gmail.com>
2025-08-30 18:34:23 +0330

    add mux and two get and post method to it

--- main.go
+++ main.go
@@ -31,10 +31,23 @@ func (s *api) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func (a *api) getUserHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Users list"))
+}
+
+func (a *api) createUserHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Created user"))
+}
+
 func main() {
 	api := &api{addr: ":8080"}
 
+	// Initialize the server mux
+	mux := http.NewServeMux()
 	srv := &http.Server{Addr: api.addr, Handler: api}
+
+	mux.HandleFunc("GET /users", api.getUserHandler)
+	mux.HandleFunc("POST /users", api.createUserHandler)
 
 	srv.ListenAndServe()
 }
