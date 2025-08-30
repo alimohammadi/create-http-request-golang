b3359a57ad2116f24fca27ed539470a93d277b2e
Ali Mohammadi <alimohammadi02321@gmail.com>
2025-08-30 19:28:53 +0330

    Seperate api and users folder

--- main.go
+++ main.go
@@ -4,50 +4,18 @@ import (
 	"net/http"
 )
 
-type api struct {
-	addr string
-}
-
-func (s *api) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from the server\n"))
-
-	switch r.Method {
-	case http.MethodGet:
-		switch r.URL.Path {
-		case "/":
-			w.Write([]byte("hello from the main route \n"))
-			return
-		case "/users":
-			w.Write([]byte("hello from the users route \n"))
-			return
-		default:
-			w.Write([]byte("404 page"))
-			return
-		}
-
-	default:
-		w.Write([]byte("404 page"))
-		return
-	}
-}
-
-func (a *api) getUserHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Users list"))
-}
-
-func (a *api) createUserHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Created user"))
-}
-
 func main() {
 	api := &api{addr: ":8080"}
 
 	// Initialize the server mux
 	mux := http.NewServeMux()
-	srv := &http.Server{Addr: api.addr, Handler: api}
+	srv := &http.Server{Addr: api.addr, Handler: mux}
 
 	mux.HandleFunc("GET /users", api.getUserHandler)
 	mux.HandleFunc("POST /users", api.createUserHandler)
 
-	srv.ListenAndServe()
+	err := srv.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
 }
